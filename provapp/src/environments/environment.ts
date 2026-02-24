5d0c431cf234380505069ad7202d656924c34fa9
francescosidoti07 <francescosidoti07@gmail.com>
2026-02-24 10:47:33 +0000

    salvataggio

---
+++ provapp/src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+    production: false,
+    keycloak: {
+        url: 'https://ubiquitous-journey-pjvvj4gv75wr296jq-8080.app.github.dev',
+        realm: 'prova',
+        clientId: 'provapp',
+        redirectUri: window.location.origin,
+    }
+};
